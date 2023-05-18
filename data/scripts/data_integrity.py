e5ede2b282f71cca32d2acafd4c4e73c5bd49853
JakubSzukala <burski09@outlook.com>
2023-05-18 09:19:52 +0200

    Fix bug that caused different md5 on different machines

--- data/scripts/data_integrity.py
+++ data/scripts/data_integrity.py
@@ -7,14 +7,17 @@ import sys
 def calculate_md5_recursive(path: Path) -> str:
     h = md5()
     for root, _, files in os.walk(path):
-        for file in files:
+        for file in sorted(files):
             filepath = os.path.join(root, file)
             with open(filepath, 'rb') as filehandle:
                 buffer = filehandle.read()
                 h.update(buffer)
+                print(f"Hexdigest: {filepath} - {h.hexdigest()} ")
     return h.hexdigest()
 
 if __name__ == '__main__':
     data_root = sys.argv[1]
     DATASET_MD5 = sys.argv[2]
-    assert calculate_md5_recursive(data_root) == DATASET_MD5
+    hexdigest = calculate_md5_recursive(data_root)
+    print("Hexdigest: ", hexdigest)
+    assert hexdigest == DATASET_MD5
