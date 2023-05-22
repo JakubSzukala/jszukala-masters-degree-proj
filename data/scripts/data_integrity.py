948db0cc97d5cf6f445dbdbf9dc0a000a4929eff
JakubSzukala <burski09@outlook.com>
2023-05-22 19:25:24 +0200

    Add progress bar

--- data/scripts/data_integrity.py
+++ data/scripts/data_integrity.py
@@ -2,12 +2,13 @@ from hashlib import md5
 from pathlib import Path
 import os
 import sys
+from tqdm import tqdm
 
 
 def calculate_md5_recursive(path: Path) -> str:
     h = md5()
     for root, _, files in os.walk(path):
-        for file in sorted(files):
+        for file in tqdm(sorted(files)):
             filepath = os.path.join(root, file)
             with open(filepath, 'rb') as filehandle:
                 buffer = filehandle.read()
@@ -17,4 +18,6 @@ def calculate_md5_recursive(path: Path) -> str:
 if __name__ == '__main__':
     data_root = sys.argv[1]
     DATASET_MD5 = sys.argv[2]
-    assert calculate_md5_recursive(data_root)== DATASET_MD5
+    md5 = calculate_md5_recursive(data_root)
+    print(f"Calculated MD5: {md5}")
+    assert md5 == DATASET_MD5
