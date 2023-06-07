1ae73f9e3fa4ca14d5e6ebded285a4983071cffd
JakubSzukala <burski09@outlook.com>
2023-06-07 14:12:12 +0200

    Implement loading dataset in adapter

---
+++ data/adapter.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+import torchvision
+
+import os
+import csv
+from pathlib import Path
+from typing import Optional, Callable, Tuple, List, Any
+from PIL import Image
+
+from data.scripts.data_integrity import calculate_md5_recursive
+import numpy as np
+
+
+def load_gwhd_df(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path)
+    df['image_id'] = df.index # Group annotations under image id
+    df['class_id'] = 0
+    df = df.assign(Box=df['BoxesString'].str.split(';')).explode('Box').reset_index(drop=True)
+    df[['x_min', 'y_min', 'x_max', 'y_max']] = df['Box'].str.split(' ', expand=True)
+    df.loc[df.loc[:, 'Box'] == 'no_box', ['Box', 'x_min', 'y_min', 'x_max', 'y_max', 'class_id']] = np.nan
+    return df
