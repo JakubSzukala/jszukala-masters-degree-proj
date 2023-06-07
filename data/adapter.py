c43fafc6b5c11d62f815715b438dabe018a0d2df
JakubSzukala <burski09@outlook.com>
2023-06-07 14:52:12 +0200

    Implement adapter class for yolo dataset

--- data/adapter.py
+++ data/adapter.py
@@ -23,6 +23,61 @@ def load_gwhd_df(path: str) -> pd.DataFrame:
     df['has_annotation'] = df.loc[:, 'Box'] != 'no_box'
     df.loc[df.loc[:, 'Box'] == 'no_box', ['Box', 'x_min', 'y_min', 'x_max', 'y_max', 'class_id', 'class_name']] = \
         [np.nan for _ in range(6)] + ['background']
+    df[['x_min', 'y_min', 'x_max', 'y_max']] = df[['x_min', 'y_min', 'x_max', 'y_max']].astype(float)
 
     return df
 
+# Source: https://towardsdatascience.com/yolov7-a-deep-dive-into-the-current-state-of-the-art-for-object-detection-ce3ffedeeaeb#0c9c 
+class GwhdToYoloAdapter(torch.utils.data.Dataset):
+    def __init__(
+            self,
+            images_dir_path: Path,
+            annotations_df: pd.DataFrame,
+            transforms: Optional[Callable] = None,
+    ):
+        self.images_dir_path = images_dir_path
+        self.annotations_df = annotations_df
+        self.transforms = transforms
+
+        # Abstraction to map image ids to image indices and other way around
+        self.image_idx_to_image_id = {
+            idx: image_id
+            for idx, image_id in enumerate(self.annotations_df.image_id.unique())
+        }
+        self.image_id_to_image_idx = {
+            v: k for k, v, in self.image_idx_to_image_id.items()
+        }
+
+
+    def __len__(self) -> int:
+        return len(self.image_idx_to_image_id)
+
+
+    def __getitem__(self, index):
+        # Compile rows representing single image from df into easier to access pieces of information
+        image_id = self.image_idx_to_image_id[index]
+        image_info = self.annotations_df[self.annotations_df.image_id == image_id]
+        file_name = image_info.image_name.values[0]
+        assert image_id == image_info.image_id.values[0]
+
+        image = Image.open(os.path.join(self.images_dir_path, file_name)).convert("RGB")
+        image = np.array(image)
+
+        image_hw = image.shape[:2]
+
+        if image_info.has_annotation.any():
+            xyxy_bboxes = image_info[["x_min", "y_min", "x_max", "y_max"]].values
+            class_ids = image_info["class_id"].values
+        else:
+            xyxy_bboxes = np.array([])
+            class_ids = np.array([])
+
+        if self.transforms is not None:
+            transformed = self.transforms(
+                image=image, bboxes=xyxy_bboxes, labels=class_ids
+            )
+            image = transformed["image"]
+            xyxy_bboxes = np.array(transformed["bboxes"])
+            class_ids = np.array(transformed["labels"])
+
+        return image, xyxy_bboxes, class_ids, image_id, image_hw
