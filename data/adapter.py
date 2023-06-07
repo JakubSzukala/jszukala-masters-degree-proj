0c450f9d24f0f85b5414a795321f5ad3894fe87a
JakubSzukala <burski09@outlook.com>
2023-06-07 14:33:21 +0200

    Add few more utility columns

--- data/adapter.py
+++ data/adapter.py
@@ -16,8 +16,13 @@ import numpy as np
 def load_gwhd_df(path: str) -> pd.DataFrame:
     df = pd.read_csv(path)
     df['image_id'] = df.index # Group annotations under image id
-    df['class_id'] = 0
+    df['class_id'] = 0 # Wheat head
+    df['class_name'] = 'wheat_head'
     df = df.assign(Box=df['BoxesString'].str.split(';')).explode('Box').reset_index(drop=True)
     df[['x_min', 'y_min', 'x_max', 'y_max']] = df['Box'].str.split(' ', expand=True)
-    df.loc[df.loc[:, 'Box'] == 'no_box', ['Box', 'x_min', 'y_min', 'x_max', 'y_max', 'class_id']] = np.nan
+    df['has_annotation'] = df.loc[:, 'Box'] != 'no_box'
+    df.loc[df.loc[:, 'Box'] == 'no_box', ['Box', 'x_min', 'y_min', 'x_max', 'y_max', 'class_id', 'class_name']] = \
+        [np.nan for _ in range(6)] + ['background']
+
     return df
+
