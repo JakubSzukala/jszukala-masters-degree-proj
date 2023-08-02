d60627e17d0563f3c46fe23cb95b4ee1e7a4631f
JakubSzukala <burski09@outlook.com>
2023-08-02 17:10:32 +0200

    Fix a bug with list indexing

--- model/utils.py
+++ model/utils.py
@@ -101,9 +101,15 @@ def detection_results_to_classification_results(gt, preds, device):
     # Assert that there are no duplicate pred indices
     assert len(preds_match_idices) == len(set(preds_match_idices))
 
+    # This fixes error: "IndexError: too many indices for tensor of dimension 1" that sometimes
+    # occurs when list is used as index
+    preds_match_idices = torch.tensor(preds_match_idices, device=device)
+
     if preds.shape[0] > gt.shape[0]:
         padding_size = preds.shape[0] - gt.shape[0]
         mask = torch.ones(preds.shape[0], dtype=torch.bool, device=device)
+        print(f"Problematic index: {preds_match_idices}")
+        print(f"Mask shape: {mask.shape}")
         mask[preds_match_idices] = False
         preds_without_matches = preds[mask, :]
         confidence_scores_padding = preds_without_matches[:, 4]
